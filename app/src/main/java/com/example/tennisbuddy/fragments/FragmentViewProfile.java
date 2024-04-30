8739a95cfcd34a4c43f91d4bf9554ff9c58d4d10
takeigh <takei001@gannon.edu>
2024-04-30 00:37:27 -0400

    Fixed Edit profile functionality

--- app/src/main/java/com/example/tennisbuddy/fragments/FragmentViewProfile.java
+++ app/src/main/java/com/example/tennisbuddy/fragments/FragmentViewProfile.java
@@ -11,46 +11,46 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.tennisbuddy.EditProfile;
 import com.example.tennisbuddy.R;
 import com.example.tennisbuddy.daos.UserDao;
 import com.example.tennisbuddy.databases.KeepUserDatabase;
 import com.example.tennisbuddy.databases.UserDatabase;
+import com.example.tennisbuddy.entities.KeepUser;
 import com.example.tennisbuddy.entities.User;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class FragmentViewProfile extends Fragment {
 
+    User viewedUser;
+    User loggedUser;
     Button logOut;
     Button editProfile;
-    TextView NameTextView;
-    TextView skillLevelTextView;
-    TextView emailTextView;
+    Button addFriend;
+    TextView name;
+    TextView skillLevel;
+    TextView email;
 
     public FragmentViewProfile() {
         // Required empty public constructor
+    }
+
+    public static FragmentViewProfile newInstance(int id) {
+        FragmentViewProfile fragment = new FragmentViewProfile();
+        Bundle args = new Bundle();
+        args.putInt("viewedUser", id);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            // Retrieve the userID from SharedPreferences
-            SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("shared_prefs", MODE_PRIVATE);
-            int userID = sharedPreferences.getInt("userID", 0); // 0 is the default value
-
-            // Get the current user's data
-            UserDatabase userDatabase = UserDatabase.getDatabase(getContext());
-            UserDao userDao = userDatabase.userDao();
-            User currentUser = userDao.getUserByID(userID);
-
-            // Set the user's data
-            String name = currentUser.getFirstName() + " " + currentUser.getLastName();
-            NameTextView.setText(name);
-            skillLevelTextView.setText(currentUser.getExperienceLevel());
-            emailTextView.setText(currentUser.getEmail());
+            viewedUser = UserDatabase.getDatabase(requireContext()).userDao().getUserByID(getArguments().getInt("viewedUser"));
         }
+        KeepUser kU = KeepUserDatabase.getDatabase(requireContext()).keepUserDao().getKeepUser();
+        loggedUser = UserDatabase.getDatabase(requireContext()).userDao().getUserByID(kU.getUserId());
     }
 
     @Override
@@ -58,33 +58,61 @@ public class FragmentViewProfile extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_profile, container, false);
 
-        NameTextView = view.findViewById(R.id.nameTextView);
-        skillLevelTextView = view.findViewById(R.id.skillLevelTextView);
-        emailTextView = view.findViewById(R.id.email);
-        logOut = view.findViewById(R.id.buttonLogOut);
-        editProfile = view.findViewById(R.id.buttonEditProfile);
+        prepComponents(view);
 
+        return view;
+    }
+
+    private void prepComponents(View view) {
+        name = view.findViewById(R.id.nameTextView);
+        String userName = viewedUser.getFirstName() + " " + viewedUser.getLastName();
+        name.setText(userName);
+
+        skillLevel = view.findViewById(R.id.skillLevelTextView);
+        skillLevel.setText(viewedUser.getExperienceLevel());
+
+        email = view.findViewById(R.id.email);
+        if (loggedUser.getUserId() == viewedUser.getUserId()) {
+            email.setVisibility(View.VISIBLE);
+            email.setText(viewedUser.getEmail());
+        } else {
+            email.setVisibility(View.GONE);
+        }
+
+        logOut = view.findViewById(R.id.buttonLogOut);
         logOut.setOnClickListener(l -> {
             KeepUserDatabase.getDatabase(requireContext()).keepUserDao().deleteUsers();
             requireActivity().finish();
         });
 
+        editProfile = view.findViewById(R.id.buttonEditProfile);
+        if (loggedUser.getUserId() == viewedUser.getUserId()) {
+            editProfile.setVisibility(View.VISIBLE);
+        } else {
+            editProfile.setVisibility(View.GONE);
+        }
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create a new instance of the EditProfile fragment
-                EditProfile editProfileFragment = new EditProfile();
+                FragmentEditProfile editProfileFragment = new FragmentEditProfile();
 
                 // Replace the current fragment with the EditProfile fragment
                 if (getActivity() != null) {
                     getActivity().getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragmentContainerMenu, editProfileFragment)
+                            .replace(R.id.fragmentContainerMain, editProfileFragment)
                             .addToBackStack(null)
                             .commit();
                 }
             }
         });
 
-        return view;
+        addFriend = view.findViewById(R.id.buttonAddFriend);
+        if (loggedUser.getUserId() == viewedUser.getUserId()) {
+            addFriend.setVisibility(View.GONE);
+        } else {
+            addFriend.setVisibility(View.VISIBLE);
+            // Need logic to add friend
+        }
     }
 }
