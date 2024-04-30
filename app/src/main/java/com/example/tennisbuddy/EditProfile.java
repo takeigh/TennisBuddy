bc37349dbd9095dfb5a34988588af280390f217f
Olive <oliverjkonzel@gmail.com>
2024-04-29 22:01:25 -0400

    Fail to implement the edit profile......

--- app/src/main/java/com/example/tennisbuddy/EditProfile.java
+++ app/src/main/java/com/example/tennisbuddy/EditProfile.java
@@ -1,5 +1,8 @@
 package com.example.tennisbuddy;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -17,50 +20,22 @@ import com.example.tennisbuddy.daos.UserDao;
 import com.example.tennisbuddy.databases.UserDatabase;
 import com.example.tennisbuddy.entities.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfile#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EditProfile extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditProfile() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditProfile newInstance(String param1, String param2) {
-        EditProfile fragment = new EditProfile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private User currentUser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // Retrieve the userID from SharedPreferences
+            SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE);
+            int userID = sharedPreferences.getInt("userID", 0); // 0 is the default value
+
+            // Get the current user's data
+            UserDatabase userDatabase = UserDatabase.getDatabase(getContext());
+            UserDao userDao = userDatabase.userDao();
+            currentUser = userDao.getUserByID(userID);
         }
     }
 
@@ -74,23 +49,24 @@ public class EditProfile extends Fragment {
         Spinner spinnerSkillLevel = view.findViewById(R.id.spinnerSkillLevel);
         Button buttonSave = view.findViewById(R.id.buttonSave);
 
+        // Show the user's data in the form
+        editFirstName.setText(currentUser.getFirstName());
+        editLastName.setText(currentUser.getLastName());
+
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String firstName = editFirstName.getText().toString();
                 String lastName = editLastName.getText().toString();
-                String skillLevel = spinnerSkillLevel.getSelectedItem().toString();
 
-                // Create a new User object with the new data
-                User user = new User();
-                user.setFirstName(firstName);
-                user.setLastName(lastName);
-                //user.setSkillLevel(skillLevel);
+                // Update the current user's data with the new data
+                currentUser.setFirstName(firstName);
+                currentUser.setLastName(lastName);
 
-                // Get the UserDao and update the user in the database
+                // Update the user's data in the database
                 UserDatabase userDatabase = UserDatabase.getDatabase(getContext());
                 UserDao userDao = userDatabase.userDao();
-                userDao.addUser(user);
+                userDao.updateUser(currentUser);
 
                 Toast.makeText(getContext(), "Profile saved!", Toast.LENGTH_SHORT).show();
             }
--- app/src/main/java/com/example/tennisbuddy/fragments/FragmentViewProfile.java
+++ app/src/main/java/com/example/tennisbuddy/fragments/FragmentViewProfile.java
@@ -1,11 +1,8 @@
 package com.example.tennisbuddy.fragments;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,21 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.example.tennisbuddy.EditProfile;
 import com.example.tennisbuddy.R;
-import com.example.tennisbuddy.activities.LandingActivity;
-import com.example.tennisbuddy.activities.LoginActivity;
 import com.example.tennisbuddy.daos.UserDao;
 import com.example.tennisbuddy.databases.KeepUserDatabase;
 import com.example.tennisbuddy.databases.UserDatabase;
-import com.example.tennisbuddy.entities.KeepUser;
-import com.example.tennisbuddy.entities.User;
-import com.example.tennisbuddy.daos.UserDao;
-import com.example.tennisbuddy.databases.UserDatabase;
 import com.example.tennisbuddy.entities.User;
 
-import java.util.Objects;
+import static android.content.Context.MODE_PRIVATE;
 
 public class FragmentViewProfile extends Fragment {
 
@@ -41,56 +32,59 @@ public class FragmentViewProfile extends Fragment {
         // Required empty public constructor
     }
 
-    public static FragmentViewProfile newInstance(String param1, String param2) {
-        FragmentViewProfile fragment = new FragmentViewProfile();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            // Retrieve the userID from SharedPreferences
+            SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("shared_prefs", MODE_PRIVATE);
+            int userID = sharedPreferences.getInt("userID", 0); // 0 is the default value
+
+            // Get the current user's data
+            UserDatabase userDatabase = UserDatabase.getDatabase(getContext());
+            UserDao userDao = userDatabase.userDao();
+            User currentUser = userDao.getUserByID(userID);
+
+            // Set the user's data
+            String name = currentUser.getFirstName() + " " + currentUser.getLastName();
+            NameTextView.setText(name);
+            skillLevelTextView.setText(currentUser.getExperienceLevel());
+            emailTextView.setText(currentUser.getEmail());
         }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_profile, container, false);
 
-        prepComponents(view);
-
-        return view;
-    }
-
-    private void prepComponents(View view) {
-        KeepUser kUser = KeepUserDatabase.getDatabase(requireContext()).keepUserDao().getKeepUser();
-        User user = UserDatabase.getDatabase(requireContext()).userDao().getUserByID(kUser.getUserId());
-
         NameTextView = view.findViewById(R.id.nameTextView);
-        String name = user.getFirstName() + " " + user.getLastName();
-        NameTextView.setText(name);
-
         skillLevelTextView = view.findViewById(R.id.skillLevelTextView);
-        skillLevelTextView.setText(user.getExperienceLevel());
-
         emailTextView = view.findViewById(R.id.email);
-        emailTextView.setText(user.getEmail());
-
         logOut = view.findViewById(R.id.buttonLogOut);
+        editProfile = view.findViewById(R.id.buttonEditProfile);
+
         logOut.setOnClickListener(l -> {
             KeepUserDatabase.getDatabase(requireContext()).keepUserDao().deleteUsers();
             requireActivity().finish();
         });
 
-        editProfile = view.findViewById(R.id.buttonEditProfile);
-    }
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of the EditProfile fragment
+                EditProfile editProfileFragment = new EditProfile();
 
-    private void addFriend() {
-        // add the friend to the current user's friend list.
-        Toast.makeText(getContext(), "Friend added!", Toast.LENGTH_SHORT).show();
+                // Replace the current fragment with the EditProfile fragment
+                if (getActivity() != null) {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerMenu, editProfileFragment)
+                            .addToBackStack(null)
+                            .commit();
+                }
+            }
+        });
+
+        return view;
     }
 }
